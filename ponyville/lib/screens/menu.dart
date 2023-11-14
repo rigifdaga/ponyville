1a2c754d3732c95202dda96d3838cc4328e04387
rigifdaga <rfdauliaa@gmail.com>
2023-11-14 13:59:50 +0700

    tugas 8 selesai

--- ponyville/lib/screens/menu.dart
+++ ponyville/lib/screens/menu.dart
@@ -1,12 +1,14 @@
 import 'package:flutter/material.dart';
+import 'package:ponyville/widgets/left_drawer.dart';
+import 'package:ponyville/widgets/shop_card.dart';
 
 class MyHomePage extends StatelessWidget {
     MyHomePage({Key? key}) : super(key: key);
 
     final List<ShopItem> items = [
-        ShopItem("Lihat Item", Icons.checklist, Colors.pink),
-        ShopItem("Tambah Item", Icons.add_shopping_cart, Colors.blue),
-        ShopItem("Logout", Icons.logout, Colors.indigo),
+        ShopItem("Lihat Produk", Icons.checklist, Colors.green),
+        ShopItem("Tambah Produk", Icons.add_shopping_cart, Colors.pink),
+        ShopItem("Logout", Icons.logout, Colors.blue),
     ];
 
     @override
@@ -16,7 +18,10 @@ class MyHomePage extends StatelessWidget {
             title: const Text(
               'Ponyville',
             ),
+            backgroundColor: Colors.indigo,
+            foregroundColor: Colors.white,
           ),
+          drawer: const LeftDrawer(),
           body: SingleChildScrollView(
             // Widget wrapper yang dapat discroll
             child: Padding(
@@ -56,56 +61,4 @@ class MyHomePage extends StatelessWidget {
           ),
         );
     }
-}
-
-class ShopItem {
-  final String name;
-  final IconData icon;
-  final Color color;
-  ShopItem(this.name, this.icon, this.color);
-}
-
-class ShopCard extends StatelessWidget {
-  final ShopItem item;
-
-  const ShopCard(this.item, {super.key}); // Constructor
-
-  @override
-  Widget build(BuildContext context) {
-    return Material(
-      color: item.color,
-      child: InkWell(
-        // Area responsive terhadap sentuhan
-        onTap: () {
-          // Memunculkan SnackBar ketika diklik
-          ScaffoldMessenger.of(context)
-            ..hideCurrentSnackBar()
-            ..showSnackBar(SnackBar(
-                content: Text("Kamu telah menekan tombol ${item.name}!")));
-        },
-        child: Container(
-          // Container untuk menyimpan Icon dan Text
-          padding: const EdgeInsets.all(8),
-          child: Center(
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Icon(
-                  item.icon,
-                  color: Colors.white,
-                  size: 30.0,
-                ),
-                const Padding(padding: EdgeInsets.all(3)),
-                Text(
-                  item.name,
-                  textAlign: TextAlign.center,
-                  style: const TextStyle(color: Colors.white),
-                ),
-              ],
-            ),
-          ),
-        ),
-      ),
-    );
-  }
 }
